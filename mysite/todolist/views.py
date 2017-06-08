36242c235e5c2db6af170bc69a32ca28867400b3
Anna <anna@twosigmas.com>
2017-06-08 16:25:34 +0100

    Initial commit of the to do list project
    Adds a task
    Searched the database
    Marks tasks as done
    Purges done tasks

---
+++ mysite/todolist/views.py
@@ -0,0 +1,59 @@
+from django import forms
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from django.template import loader
+from django.urls import reverse
+
+from .models import Task
+from .submit import  save_submission, edit_submission, complete_task
+
+
+
+def index(request):
+    print(request.POST)
+    todo_list = Task.objects.all()
+    waiting = todo_list.filter(completed=False)
+    done = todo_list.filter(completed=True)
+    
+
+    if request.POST.get("submit_task"):
+        content = (request.POST['text_field'])
+        if content != "":
+            if not Task.objects.filter(task_text=content).exists():
+                save_submission(content)
+
+    if request.POST.get("complete_task"):
+        id_to_complete = (request.POST['task_id'])
+        print (id_to_complete)
+        # complete_task(id_to_complete)
+        Task.objects.filter(id=id_to_complete).update(completed = True)
+    
+    if request.POST.get("delete_submission"):
+        id_to_complete = (request.POST['task_id'])
+        print (id_to_complete)
+        print (Task.objects.filter(id=id_to_complete))
+        Task.objects.filter(id=id_to_complete).delete()
+
+# bonus comment to try git
+    
+    # if request.POST.get("edit_task"):
+    #     id_to_edit = (request.POST['task_id'])
+    #     content = (request.POST['text_field'])
+    #     print (id_to_edit)
+    #     print (content)        
+    #     edit_submissions(id_to_edit)
+          
+    if request.POST.get("search_tasks"):
+        content = (request.POST['text_field'])
+
+        if content != "":  
+            print (content)            
+            print (Task.objects.filter(task_text__icontains=content))
+            todo_list = todo_list.filter(task_text__icontains=content)
+    
+    context = {
+        'waiting': waiting,
+        'done' : done, 
+    }
+    return render(request, 'todolist/index.html', context)
+
